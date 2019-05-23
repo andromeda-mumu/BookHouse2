761a8378fd6e56e741299c7a02db39107381e835
wang jiao <1624458215@qq.com>
2019-05-23 18:53:43 +0800

    折叠布局实践

--- app/src/main/java/com/example/mmc/bookhouse/ui/activity/FloatingActivityAuto.java
+++ app/src/main/java/com/example/mmc/bookhouse/ui/activity/FloatingActivityAuto.java
@@ -1,13 +1,24 @@
 package com.example.mmc.bookhouse.ui.activity;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 
 import com.example.mmc.bookhouse.R;
+import com.example.mmc.bookhouse.adapter.HomePagerAdapter;
+import com.example.mmc.bookhouse.ui.base.BaseFragment;
+import com.example.mmc.bookhouse.ui.fragment.AddBookFragment;
+import com.example.mmc.bookhouse.ui.fragment.BookFragment;
+import com.example.mmc.bookhouse.ui.fragment.SearchFragment;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * 系统自动生成
@@ -15,20 +26,43 @@ import com.example.mmc.bookhouse.R;
 
 public class FloatingActivityAuto extends AppCompatActivity {
 
+    @BindView(R.id.toolbar)
+    Toolbar mToolbar;
+    @BindView(R.id.toolbar_layout)
+    CollapsingToolbarLayout mToolbarLayout;
+    @BindView(R.id.app_bar)
+    AppBarLayout mAppBar;
+    @BindView(R.id.viewpager)
+    ViewPager mViewpager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_floating_auto);
+        ButterKnife.bind(this);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        //        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        //        fab.setOnClickListener(new View.OnClickListener() {
+        //            @Override
+        //            public void onClick(View view) {
+        //                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+        //                        .setAction("Action", null).show();
+        //            }
+        //        });
+
+
+        initView();
+    }
+
+    private void initView() {
+        List<BaseFragment> fragmentList = new ArrayList<>();
+        fragmentList.add(new BookFragment());
+        fragmentList.add(new SearchFragment());
+        fragmentList.add(new AddBookFragment());
+        HomePagerAdapter adapter = new HomePagerAdapter(getSupportFragmentManager(), fragmentList);
+        mViewpager.setAdapter(new HomePagerAdapter(getSupportFragmentManager(),fragmentList));
+
     }
 }
