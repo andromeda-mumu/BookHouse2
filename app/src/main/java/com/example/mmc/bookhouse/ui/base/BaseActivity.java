65bf751c73875807c612523df481b2fcb45b39b0
wang jiao <1624458215@qq.com>
2019-05-23 18:33:12 +0800

    toolbar和折叠布局学习

--- app/src/main/java/com/example/mmc/bookhouse/ui/base/BaseActivity.java
+++ app/src/main/java/com/example/mmc/bookhouse/ui/base/BaseActivity.java
@@ -4,17 +4,22 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import butterknife.ButterKnife;
 
 /**
  * Created by wangjiao on 2019/5/22.
  * 功能描述：activity基类
  */
 
-public abstract  class BaseActivity extends Activity{
+public abstract  class BaseActivity extends AppCompatActivity{
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(getResId());
+        ButterKnife.bind(this);
+        initView();
         initData();
         initListener();
     }
@@ -22,10 +27,12 @@ public abstract  class BaseActivity extends Activity{
     protected abstract int getResId();
     protected void initData(){};
     protected void initListener(){};
+    protected void initView(){}
 
     public void startActivity(Class<Activity> clazz){
         Intent intent = new Intent(this,clazz);
         startActivity(intent);
     }
+
 
 }
