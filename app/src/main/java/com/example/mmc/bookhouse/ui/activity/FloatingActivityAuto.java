d9e55202f2687babbb75dcfb91fe3170f43a9e56
wang jiao <1624458215@qq.com>
2019-05-23 19:15:07 +0800

    折叠布局更深实践

--- app/src/main/java/com/example/mmc/bookhouse/ui/activity/FloatingActivityAuto.java
+++ app/src/main/java/com/example/mmc/bookhouse/ui/activity/FloatingActivityAuto.java
@@ -1,11 +1,14 @@
 package com.example.mmc.bookhouse.ui.activity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.example.mmc.bookhouse.R;
 import com.example.mmc.bookhouse.adapter.HomePagerAdapter;
@@ -13,6 +16,7 @@ import com.example.mmc.bookhouse.ui.base.BaseFragment;
 import com.example.mmc.bookhouse.ui.fragment.AddBookFragment;
 import com.example.mmc.bookhouse.ui.fragment.BookFragment;
 import com.example.mmc.bookhouse.ui.fragment.SearchFragment;
+import com.example.mmc.bookhouse.utils.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -37,6 +41,7 @@ public class FloatingActivityAuto extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_floating_auto);
         ButterKnife.bind(this);
@@ -53,8 +58,53 @@ public class FloatingActivityAuto extends AppCompatActivity {
         //        });
 
 
+        initToolbar();
         initView();
     }
+
+    private void initToolbar() {
+        mToolbarLayout.setTitle("toolbarlayout"); //这才是真正名字的地方
+
+        mToolbar.setNavigationIcon(R.drawable.drawer);
+        //        mToolbar.setLogo(R.drawable.orange_icon);
+        mToolbar.setTitle("BookHouse");
+        mToolbar.setLogo(android.R.drawable.ic_menu_add);
+        mToolbar.setTitleTextColor(Color.WHITE);
+        mToolbar.setSubtitle("毛毛虫");
+        mToolbar.setSubtitleTextColor(Color.WHITE);
+        setSupportActionBar(mToolbar);
+
+        mToolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                int menuItemId = item.getItemId();
+                switch(menuItemId){
+                    case R.id.action_search:
+                        com.example.mmc.bookhouse.utils.Toast.show("搜索");
+                        break;
+                    case R.id.action_notification:
+                        Toast.show("通知");
+                        break;
+                    case R.id.action_item1:
+                        Toast.show("item1");
+                        break;
+                    case R.id.action_item2:
+                        Toast.show("item2");
+                        break;
+                }
+                return true;
+            }
+        });
+    }
+
+
+    //这会自动加到toolbar上
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.base_toolbar_menu,menu);
+        return true;
+    }
+
 
     private void initView() {
         List<BaseFragment> fragmentList = new ArrayList<>();
