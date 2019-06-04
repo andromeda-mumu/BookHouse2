84af8d4dc21a2b1c9acc1225227008ba83bd1b42
wang jiao <1624458215@qq.com>
2019-06-04 16:52:01 +0800

    规范

--- app/src/main/java/com/example/mmc/bookhouse/MainActivity.java
+++ app/src/main/java/com/example/mmc/bookhouse/MainActivity.java
@@ -3,15 +3,12 @@ package com.example.mmc.bookhouse;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
-import android.util.Log;
 import android.view.View;
 import android.widget.LinearLayout;
 
@@ -32,8 +29,6 @@ import com.example.mmc.bookhouse.utils.ScreenUtils;
 import com.example.mmc.bookhouse.utils.SharePreferentUtils;
 import com.example.mmc.bookhouse.utils.Toast;
 import com.example.mmc.bookhouse.view.ImageTextView;
-import com.liyu.sqlitetoexcel.ExcelToSQLite;
-import com.liyu.sqlitetoexcel.SQLiteToExcel;
 
 import org.greenrobot.eventbus.Subscribe;
 import org.greenrobot.eventbus.ThreadMode;
@@ -74,90 +69,10 @@ public class MainActivity extends FragmentActivity {
         initTypeTable();
         initView();
         initData();
-        initListener();
-//        sqlToExcel();
-        excelToSql();
+//        initListener();
     }
 
-    private void showDbMsg(String dbName) {
-        SQLiteDatabase database;
-        try {
-            database = SQLiteDatabase.openOrCreateDatabase(dbName, null);
-            Cursor cursor = database.rawQuery("select name from sqlite_master where type='table' order by name", null);
-            while (cursor.moveToNext()) {
-                Log.d("=mmc=","New tables is : " + cursor.getString(0) + "  ");
-                Cursor cursor2 = database.rawQuery("select * from " + cursor.getString(0), null);
-                while (cursor2.moveToNext()) {
-                    Log.d("=mmc=","\n");
-                    for (int i = 0; i < cursor2.getColumnCount(); i++) {
-                        Log.d("=mmc=",cursor2.getString(i) + "  ");
-                    }
-                }
-                cursor2.close();
-            }
-            cursor.close();
-            database.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
 
-    /**
-     * 外部excel表格转换成sqlite数据
-     */
-    private void excelToSql() {
-        new ExcelToSQLite
-                .Builder(this)
-                                .setDataBase(getDatabasePath("BookDatabase.db").getPath())
-                .setAssetFileName("book.xls")
-                //                .setFilePath(outputFile)
-                .setDecryptKey("1234567")
-                .setDateFormat("yyyy-MM-dd HH:mm:ss")
-                .start(new ExcelToSQLite.ImportListener() {
-                    @Override
-                    public void onStart() {
-                        Log.d("=mmc=","----start----");
-                    }
-
-                    @Override
-                    public void onCompleted(String result) {
-                        Log.d("=mmc=","Import completed--->" + result);
-                        showDbMsg(result);
-                    }
-
-                    @Override
-                    public void onError(Exception e) {
-                        Log.d("=mmc=","-----error---"+e.getMessage());
-                    }
-                });
-    }
-
-    /**
-     * 数据库转excel,并在外部存储中可以导出查看
-     * 手机也能直接查看
-     */
-    private void sqlToExcel() {
-        new SQLiteToExcel.Builder(this)
-                .setDataBase(getDatabasePath("BookDatabase.db").getPath())
-                .setTables("BookType")
-//                .setEncryptKey("1234567")
-//                .setProtectKey("9876543")
-                .start(new SQLiteToExcel.ExportListener() {
-                    @Override
-                    public void onStart() {
-                    }
-
-                    @Override
-                    public void onCompleted(String filePath) {
-                        Log.d("=mmc=", "Export completed--->" + filePath);
-                    }
-
-                    @Override
-                    public void onError(Exception e) {
-                    }
-                });
-
-    }
 
     private void checkPermission() {
         if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED){
--- app/src/main/java/com/example/mmc/bookhouse/ui/fragment/ImpressionFragment.java
+++ app/src/main/java/com/example/mmc/bookhouse/ui/fragment/ImpressionFragment.java
@@ -1,7 +1,14 @@
 package com.example.mmc.bookhouse.ui.fragment;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
+import android.view.View;
+
 import com.example.mmc.bookhouse.R;
 import com.example.mmc.bookhouse.ui.base.BaseFragment;
+import com.liyu.sqlitetoexcel.ExcelToSQLite;
+import com.liyu.sqlitetoexcel.SQLiteToExcel;
 
 /**
  * Created by wangjiao on 2019/5/22.
@@ -13,4 +20,89 @@ public class ImpressionFragment extends BaseFragment {
     protected int getLayoutId() {
         return R.layout.fragment_impression;
     }
+
+    /**
+     * xls->sql
+     * 外部excel表格转换成sqlite数据
+     * @param view
+     */
+    public void doImport(View view){
+        new ExcelToSQLite
+                .Builder(activity())
+                .setDataBase(activity().getDatabasePath("BookDatabase.db").getPath())
+                .setAssetFileName("book.xls")
+                //                .setFilePath(outputFile)
+                .setDecryptKey("1234567")
+                .setDateFormat("yyyy-MM-dd HH:mm:ss")
+                .start(new ExcelToSQLite.ImportListener() {
+                    @Override
+                    public void onStart() {
+                        Log.d("=mmc=","----start----");
+                    }
+
+                    @Override
+                    public void onCompleted(String result) {
+                        Log.d("=mmc=","Import completed--->" + result);
+//                        showDbMsg(result);
+                    }
+
+                    @Override
+                    public void onError(Exception e) {
+                        Log.d("=mmc=","-----error---"+e.getMessage());
+                    }
+                });
+    }
+
+    /**
+     * sql -> xls
+     * 数据库转excel,并在外部存储中可以导出查看
+     * 手机也能直接查看
+     * @param view
+     */
+    public void doExport(View view){
+        new SQLiteToExcel.Builder(activity())
+                .setDataBase(activity().getDatabasePath("BookDatabase.db").getPath())
+                .setTables("BookType")
+                //                .setEncryptKey("1234567")
+                //                .setProtectKey("9876543")
+                .start(new SQLiteToExcel.ExportListener() {
+                    @Override
+                    public void onStart() {
+                    }
+
+                    @Override
+                    public void onCompleted(String filePath) {
+                        Log.d("=mmc=", "Export completed--->" + filePath);
+                    }
+
+                    @Override
+                    public void onError(Exception e) {
+                    }
+                });
+    }
+    private void showDbMsg(String dbName) {
+        SQLiteDatabase database;
+        try {
+            database = SQLiteDatabase.openOrCreateDatabase(dbName, null);
+            Cursor cursor = database.rawQuery("select name from sqlite_master where type='table' order by name", null);
+            while (cursor.moveToNext()) {
+                Log.d("=mmc=","New tables is : " + cursor.getString(0) + "  ");
+                Cursor cursor2 = database.rawQuery("select * from " + cursor.getString(0), null);
+                while (cursor2.moveToNext()) {
+                    Log.d("=mmc=","\n");
+                    for (int i = 0; i < cursor2.getColumnCount(); i++) {
+                        Log.d("=mmc=",cursor2.getString(i) + "  ");
+                    }
+                }
+                cursor2.close();
+            }
+            cursor.close();
+            database.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
 }
