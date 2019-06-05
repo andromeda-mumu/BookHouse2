5c46d59fe8a172414c77fff90b1b33940649fe63
wang jiao <1624458215@qq.com>
2019-06-05 11:04:59 +0800

    备份数据，讲sql导出成xls

--- app/src/main/java/com/example/mmc/bookhouse/ui/fragment/ImpressionFragment.java
+++ app/src/main/java/com/example/mmc/bookhouse/ui/fragment/ImpressionFragment.java
@@ -1,10 +1,19 @@
 package com.example.mmc.bookhouse.ui.fragment;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.mmc.bookhouse.R;
 import com.example.mmc.bookhouse.ui.base.BaseFragment;
 import com.example.mmc.bookhouse.utils.SqliteAndXlsUtils;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.Unbinder;
 
 /**
  * Created by wangjiao on 2019/5/22.
@@ -12,17 +21,39 @@ import com.example.mmc.bookhouse.utils.SqliteAndXlsUtils;
  */
 
 public class ImpressionFragment extends BaseFragment {
+    @BindView(R.id.export)
+    Button mExport;
+    @BindView(R.id.btn_import)
+    Button mBtnImport;
+    Unbinder unbinder;
+
     @Override
     protected int getLayoutId() {
         return R.layout.fragment_impression;
     }
 
+    @OnClick({R.id.export,R.id.btn_import})
+    public void onClick(View view){
+        switch(view.getId()){
+            case R.id.export:
+                doExport();
+                break;
+            case  R.id.btn_import:
+                doImport();
+                break;
+
+            default :
+                break;
+
+        }
+    }
+
     /**
      * xls->sql
      * 外部excel表格转换成sqlite数据
-     * @param view
+     *
      */
-    public void doImport(View view){
+    public void doImport() {
         SqliteAndXlsUtils.excelToSqlite();
     }
 
@@ -30,10 +61,22 @@ public class ImpressionFragment extends BaseFragment {
      * sql -> xls
      * 数据库转excel,并在外部存储中可以导出查看
      * 手机也能直接查看
-     * @param view
      */
-    public void doExport(View view){
-       SqliteAndXlsUtils.sqliteToExcel();
+    public void doExport() {
+        SqliteAndXlsUtils.sqliteToExcel();
     }
 
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // TODO: inflate a fragment view
+        View rootView = super.onCreateView(inflater, container, savedInstanceState);
+        unbinder = ButterKnife.bind(this, rootView);
+        return rootView;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+    }
 }
